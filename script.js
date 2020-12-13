8129f5e530da1be84fd12ec9919bed871378d4c0
That Guy Cal <calmanning@gmail.com>
2020-12-13 13:49:01 -0800

    re-worked the script. Functionally, we're still at the same place, but the code has be cleaned up a bit. Having dificulties getting the 3rd question set to populate.

--- script.js
+++ script.js
@@ -1,73 +1,104 @@
-// TODO: Create arrays for all question sets that will cycle in on the same page.
-var promptArray = ["This would be a sample question promptArray.","And this would be the second one.","which makes this the third prompt."];
-var answersArray1 = ["one","two","three","founr...yep"];
-var promptArray2 = "";
-var answersArray2 = [];
-var promptArray3 = "";
-var answersArray3 = [];
-var promptArray4 = "";
-var answersArray4 = [];
-var quizEl = document.querySelector("#quiz");
-var promptEL = document.querySelector("#prompt");
-var answerEl = document.querySelector("#answers");
-// TODO: create variables for all dynamic elements present in the HTML
-// Button that starts the test.
-var startEl = document.querySelector("#start");
-    // TODO:function that clears most elements from the page and presents the prompt arrays
-    
-    // TODO: make use of HTML "data-" tags to switch visability for each question
+var buttonEl = document.querySelector(".button"); 
+var introTextEl = document.querySelector("#intro");
+var questEl = document.querySelector("#quest")
+var ulEl = document.querySelector("#answers")
+var pEl = document.querySelector("#response");
 
-        // TODO: Things that will need visability toggling: h1 intro header, h2 prompt line, ul "answers"
-
-// TODO: A function that, once the correct answer is "clicked" will clear out the old question set and install the next one.
-    // TODO: Will need a for loop to populate each li in the prompt arrays
+var answerLi = "";
+var answerButton = "";
 
 
-console.log(promptArray);
-console.log(answersArray1);
-if (startEl === true) {
-    // renderPrompt();
-    // renderAnswersArray1();
+var question1 = "What is an Array";
+var answerOne = ["yes","no","sure","maybe?"]
+var question2 = "What is JS"
+var answerTwo = ["1","2no","3sure","definitely maybe?"]
+var question3 = "something" 
+var answerThree = ["yes","no","sure","maybe?"]
+
+function buttonClicked () {
+    introTextEl.textContent = ""
+    buttonEl.style.display = "none"
+    questEl.textContent = question1
+    for (var i = 0; i < answerOne.length; i++) {
+        var answerOneText = answerOne[i];
+        console.log(i);
+        console.log(answerOneText);
+
+        var answerLi = document.createElement("li");
+        var answerButton = document.createElement("button");
+        answerButton.textContent = answerOneText;
+        answerLi.appendChild(answerButton);
+        ulEl.appendChild(answerLi);        
+    }      
 }
 
-    function renderPrompt(){
-// innerHTML clear?
 
-    for (var i = 0; i < promptArray.length; i++) {
-        var promptText = promptArray[i];
+function questionTwo() {
+questEl.textContent = question2
+answers.textContent = "";
 
-console.log(`promptArray: ${promptArray[i]}`);
-       
-        var h2 = document.createElement("h2");
-        h2.textContent = promptText;
-        quizEl.appendChild(h2);
-        }
-        
-    }
-    function renderAnswers(){
-        // innerHTML clear?
-        
-            for (var i = 0; i < answersArray1.length; i++) {
-                var answerText = answersArray1[i];
-        
-        console.log(`answersArray1: ${answersArray1[i]}`);
-               
-                var li = document.createElement("li");
-                var answerButton = document.createElement("button");
-                answerButton.textContent = answerText;
-                li.appendChild(answerButton);
-                answerEl.appendChild(li);
-                }
-                
-    }
+for (var i = 0; i < answerTwo.length; i++) {
+    var answerTwoText = answerTwo[i];
+    console.log(i);
+    console.log(answerTwoText);
 
-  
     
+    var answerLi = document.createElement("li");
+    var answerButton = document.createElement("button");
+    answerButton.textContent = answerTwoText;
+    answerLi.appendChild(answerButton);
+    ulEl.appendChild(answerLi);
+    
+}      
+}
 
-// TODO:function that clears most elements from the page and presents the prompt arrays
-startEl.addEventListener("click", function(event) {
-    console.log("uhhh, 'click'")
-   // TODO: An on page notification that the correct/incorrect answer was selected 
-   renderAnswers(); 
-   renderPrompt();
+// add target match
+if (answerButton === "maybe?") {
+questionTwo();
+} else {
+    // var wrong = document.createElement("p")
+    // wrong.textContent = "sorry. Try again, buddy";
+    // pEl.append(wrong);
+    // // timer subtract
+}
+
+function questionThree() {
+    questEl.textContent = question3
+    answers.textContent = "";
+    
+    for (var i = 0; i < answerTwo.length; i++) {
+        var answerTwoText = answerTwo[i];
+        console.log(i);
+        console.log(answerTwoText);
+    
+        
+        var answerLi = document.createElement("li");
+        var answerButton = document.createElement("button");
+        answerButton.textContent = answerTwoText;
+        answerLi.appendChild(answerButton);
+        ulEl.appendChild(answerLi);
+        
+    }      
+    }
+
+if (answerButton === "2no") {
+questionThree();
+} else {
+    // var wrong = document.createElement("p")
+    // wrong.textContent = "sorry. Try again, buddy";
+    // pEl.append(wrong);
+    // // timer subtract
+}
+buttonEl.addEventListener("click", function(event) {
+    event.preventDefault
+    console.log("uhhh, 'click'");
+    buttonClicked()
+    
+})
+
+ulEl.addEventListener("click", function(event) { 
+    event.preventDefault
+    console.log("uhhh, 'click'");
+    questionTwo();
+
 })
